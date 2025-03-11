fc1040dea8f2f588332011bed69b0d8ebcebc0fa
kt474 <kt474@cornell.edu>
2025-03-10 22:21:55 -0400

    Add lastUpdated

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "ffwrapped",
   description: "A fantasy football blog",
-
+  lastUpdated: true,
   cleanUrls: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
