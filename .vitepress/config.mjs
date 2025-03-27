77bb6b0a549c9d8c1ca0d54b53e0960d05d75ddd
kt474 <kt474@cornell.edu>
2025-03-27 18:35:45 -0400

    add home back to sidebar

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     sidebar: [
+      { text: "Home", link: "/" },
       {
         text: "Posts",
         items: [
